1d54bd120d497ac0b96dfb3b5124cef37d9bc25e
athanasakosangelos@gmail.com <athanasakosangelos@gmail.com>
2017-10-12 15:36:21 +0100

    Added index, upload templates and draft functionalit of writing data to static file

--- castles.py
+++ castles.py
@@ -1,14 +1,15 @@
-from flask  import Flask, url_for, abort, request, render_template
+from flask  import Flask, url_for, abort, request, render_template, json
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  name = request.args.get('name', '')
-  if name == '':
-    return "no param supplied"
-  else:
-    return "Hello %s" % name
+def root():
+    return render_template('index.html'), 200
 
+@app.route("/upload", methods=['POST', 'GET'])
+def upload():
+    mdict = {'default' : u'', 'required': u'on', 'name': u'short_text', 'name': u'name', 'submit': u'submit'}
+    json_value = json.dumps(mdict, separators=(',',':'))
+    return render_template('upload.html'), 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
