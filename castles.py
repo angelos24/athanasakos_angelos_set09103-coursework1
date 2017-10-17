9ccb238eab36d27e18da286107ee0c701d14391a
athanasakosangelos@gmail.com <athanasakosangelos@gmail.com>
2017-10-17 22:27:31 +0100

    Added new template pages, custom 404 and 401 and login session

--- castles.py
+++ castles.py
@@ -1,13 +1,43 @@
-from flask  import Flask, url_for, abort, request, render_template, json, flash, redirect
+from flask  import Flask, url_for, abort, request, render_template, json, flash, redirect, session
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+@app.errorhandler(401)
+def http_error_handler(error):
+        return render_template('401.html', error=error), error.code
+
+@app.errorhandler(404)
+def http_error_handler(error):
+    return render_template('404.html', error=error), error.code
+
 @app.route("/")
 def root():
-    return render_template('index.html'), 200
+        return render_template('index.html'), 200
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            flash('Invalid Credentials. Please try again.')
+        else:
+            session['logged_in'] = True
+            session['username'] = request.form['username']
+            return redirect(url_for('root'))
+    return render_template('login.html')
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    flash('<h1>You have successfully logged out!</h1>')
+    return redirect(url_for('root')), 200
+
 
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
+
+    if not session.get('logged_in'):
+        return abort(401)
+
     if request.method == 'POST':
         name = request.form['castle']
         clan = request.form['clan']
@@ -26,11 +56,24 @@ def upload():
     else :
         return render_template('upload.html'), 200
 
-@app.route("/results")
+
+@app.route("/regions")
+def regions():
+    if not session.get('logged_in'):
+        return abort(401)
+    else:
+        with open("regions.json","r") as f:
+            parent_dict = json.load(f)
+            return render_template("regions.html", parent_dict=parent_dict), 200
+
+@app.route("/castles")
 def results():
-    with open("text.json","r") as f:
-        parent_dict = json.load(f)
-    return render_template("results.html", parent_dict=parent_dict), 200
+    if not session.get('logged_in'):
+        return abort(401)
+    else:
+        with open("castles.json","r") as f:
+            parent_dict = json.load(f)
+            return render_template("castles.html", parent_dict=parent_dict), 200
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
