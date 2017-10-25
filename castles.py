c94bbfb52e4179932d92531cdb07cc34c6cb8266
athanasakosangelos@gmail.com <athanasakosangelos@gmail.com>
2017-10-25 11:40:03 +0100

    Added comments

--- castles.py
+++ castles.py
@@ -2,18 +2,22 @@ from flask  import Flask, url_for, abort, request, render_template, json, flash,
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
+# Custom 401
 @app.errorhandler(401)
 def http_error_handler(error):
         return render_template('401.html', error=error), error.code
 
+# Custom 404
 @app.errorhandler(404)
 def http_error_handler(error):
     return render_template('404.html', error=error), error.code
 
+# Web root
 @app.route("/")
 def root():
         return render_template('index.html'), 200
 
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -25,13 +29,14 @@ def login():
             return redirect(url_for('root')), 303
     return render_template('login.html'), 200
 
+# Logout page
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     flash('<div class="alert alert-success" role="alert"> <strong>Well done!</strong> You have successfully logged out</div>')
     return redirect(url_for('root')), 303
 
-
+# Add a castle page
 @app.route("/upload", methods=['POST', 'GET'])
 def upload():
 
@@ -48,6 +53,7 @@ def upload():
         castles = []
         castles.append({'Castle': name,'Clan': clan, 'Region': region, 'Date': date, 'Description': description})
 
+        # Code for replace "[" with "," when appending new data
         f2 = open("castles.json","r+")
         f2.seek(-2,2)
         f2.write(json.dumps(castles, indent=4).replace('[',',',1))
@@ -59,7 +65,7 @@ def upload():
     else :
         return render_template('upload.html'), 200
 
-
+# Regions page
 @app.route("/regions/")
 def regions():
     if not session.get('logged_in'):
@@ -67,11 +73,13 @@ def regions():
     else:
         return render_template("regions.html"), 200
 
+# Castle Regions page
 @app.route("/regions/<path:castle_region>")
 def choose_regions(castle_region):
     regions = {'highlands':1,'lowlands':2}
     if not session.get('logged_in'):
         return abort(401)
+    
     if castle_region in regions:
         with open("regions.json","r") as f:
             parent_dict = json.load(f)
@@ -79,6 +87,7 @@ def choose_regions(castle_region):
     else:
         return abort(404)
 
+# Castles page
 @app.route("/castles")
 def results():
     if not session.get('logged_in'):
